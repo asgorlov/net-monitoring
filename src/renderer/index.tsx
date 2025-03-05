cb2d5b7eaaa9c08c9c38d64dc662e220992b9239
GorlovAS <gorlovas@rtk-cd.ru>
2025-03-05 14:11:01 +0300

    [Feature] Добавлен redux-toolkit

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 // toDo:
 //  2) добавить redux-toolkit
 //  3) добавить sass
+//  4) добавить antd + работа с svg
 
 const appElement: HTMLElement = document.getElementById("app");
 if (appElement) {
